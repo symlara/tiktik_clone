2fbaf1595217977ef7fbac88c70dbb9c4ba01a7e
Aaliyah <40181569+symlara@users.noreply.github.com>
2022-09-02 22:30:13 -0500

    first commit

---
+++ pages/api/like.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { v4 as uuid_v4 } from "uuid";
+
+import { allUsersQuery } from "./../../utils/queries";
+import { client } from "../../utils/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PUT") {
+    const { userId, postId, like } = req.body;
+
+    const data = like
+      ? await client
+          .patch(postId)
+          .setIfMissing({ likes: [] })
+          .insert("after", "likes[-1]", [
+            {
+              _key: uuid_v4(),
+              _ref: userId,
+            },
+          ])
+          .commit()
+      : await client
+          .patch(postId)
+          .unset([`likes[_ref=="${userId}"]`])
+          .commit();
+
+    res.status(200).json(data);
+  }
+}
